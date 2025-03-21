45dc6523ee82baa2f4784546a2db864f7f3ed289
aragami3070 <smirnov17612@gmail.com>
2025-03-22 01:03:39 +0400

    Fix: Services for new User entity

--- Services/Implementations/UserServices.cs
+++ Services/Implementations/UserServices.cs
@@ -67,6 +67,10 @@ public class UserServices : IUserServices
     {
         // Хэширование Password
         userEntity = _HashingServices.Hashing(userEntity);
+        userEntity.Role = "Student";
+        userEntity.Points = 0;
+        userEntity.FinishedRequests = 0;
+        userEntity.CurrentRequests = "";
         // Создаем User
         await _UserRepository.Create(userEntity);
         var baseResponse = BaseResponse<bool>.Created("User created");
@@ -96,10 +100,8 @@ public class UserServices : IUserServices
         if (user != null)
         {
             // Удаляем User из кеша по Email
-
             _CachingServices.RemoveAsync(userInCache.Email);
         }
-
         // User не найден (404)
         if (user == null)
         {
@@ -172,8 +174,8 @@ public class UserServices : IUserServices
         // User found
         user.Email = userEntity.Email;
         user.Password = userEntity.Password;
-        user.FirstName = userEntity.FirstName;
-        user.SecondName = userEntity.SecondName;
+        user.Name = userEntity.Name;
+        user.Group = userEntity.Group;
 
         // User edit (201)
         await _UserRepository.Update(user);
