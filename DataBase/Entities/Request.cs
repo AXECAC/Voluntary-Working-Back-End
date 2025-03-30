c3dffc6e4e9cb8adb100f96b13cccf811baac6a8
aragami3070 <smirnov17612@gmail.com>
2025-03-30 21:07:13 +0400

    Fix: Request entity field

--- DataBase/Entities/Request.cs
+++ DataBase/Entities/Request.cs
@@ -10,7 +10,7 @@ public class Request
     public DateTime DeadLine { get; set; }
     public int PointNumber { get; set; }
     public int NeededPeopleNumber { get; set; }
-    public int[] ResponsedPeople { get; set; }
+    public int[] RespondedPeople { get; set; }
     public string Desctiption { get; set; }
     public bool IsComplited { get; set; }
     public bool IsFailed { get; set; }
