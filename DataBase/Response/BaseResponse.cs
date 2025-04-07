8250455ef61d7b1e99544d3a6b65795fe5ceaaad
aragami3070 <smirnov17612@gmail.com>
2025-04-07 13:00:24 +0400

    Add: implementation for IBaseResponse without Data field

--- DataBase/Response/BaseResponse.cs
+++ DataBase/Response/BaseResponse.cs
@@ -1,6 +1,6 @@
 namespace DataBase;
 
-// Class BaseResponse
+// Класс BaseResponse
 public class BaseResponse<T> : IBaseResponse<T>
 {
     public string Description { get; set; }
@@ -95,6 +95,85 @@ public class BaseResponse<T> : IBaseResponse<T>
     public static BaseResponse<T> InternalServerError(string description = "")
     {
         return new BaseResponse<T>()
+        {
+            StatusCode = StatusCodes.InternalServerError,
+            Description = description,
+        };
+    }
+}
+// Класс BaseResponse
+public class BaseResponse : IBaseResponse
+{
+    public string Description { get; set; }
+
+    public StatusCodes StatusCode { get; set; }
+
+
+    // Генерация Ok response  (200)
+    public static BaseResponse Ok(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.Ok,
+            Description = description,
+        };
+    }
+
+    // Генерация Created response (201)
+    public static BaseResponse Created(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.Created,
+            Description = description,
+        };
+    }
+
+
+    // NoContent response (204)
+    public static BaseResponse NoContent(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.NoContent,
+            Description = description,
+        };
+    }
+
+    // Генерация Unauthorized response (401)
+    public static BaseResponse Unauthorized(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.Unauthorized,
+            Description = description,
+        };
+    }
+
+    // Генерация NotFound response (404)
+    public static BaseResponse NotFound(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.NotFound,
+            Description = description,
+        };
+    }
+
+    // Генерация Conflict response (409)
+    public static BaseResponse Conflict(string description = "")
+    {
+        return new BaseResponse()
+        {
+            StatusCode = StatusCodes.Conflict,
+            Description = description,
+        };
+    }
+
+    // Генерация InternalServerError response (500)
+    public static BaseResponse InternalServerError(string description = "")
+    {
+        return new BaseResponse()
         {
             StatusCode = StatusCodes.InternalServerError,
             Description = description,
