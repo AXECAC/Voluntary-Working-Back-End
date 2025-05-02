480522cd90b60682183f0105c4655ebdccc9087f
aragami3070 <smirnov17612@gmail.com>
2025-05-02 16:29:38 +0400

    Add: SetRespondedPeople to PublicRequest

--- Services/Classes/PublicRequest.cs
+++ Services/Classes/PublicRequest.cs
@@ -22,8 +22,26 @@ public class PublicRequest
         this.DeadLine = request.DeadLine;
         this.PointNumber = request.PointNumber;
         this.NeededPeopleNumber = request.NeededPeopleNumber;
-        // this.RespondedPeople = request.RespondedPeople.Count();
+        this.RespondedPeople = 0;
         this.Description = request.Description;
         this.IsComplited = request.IsComplited;
+    }
+
+    // Устанавливаем количество откликнувшихся User
+    public void SetRespondedPeople(List<RespondedPeople> respondedPeople)
+    {
+        // Обнуляем
+        this.RespondedPeople = 0;
+        // Считаем
+        for (int i = 0; i < respondedPeople.Count; ++i)
+        {
+            // Если Id PublicRequest и RequestId совпадают
+            if (this.Id == respondedPeople[i].RequestId)
+            {
+                // Увеличиваем количество людей
+                this.RespondedPeople += 1;
+            }
+        }
+
     }
 }
