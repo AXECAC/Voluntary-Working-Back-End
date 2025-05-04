820e60d36bacf538b1c23282061fcfd8fe5ec636
aragami3070 <smirnov17612@gmail.com>
2025-05-05 01:33:10 +0400

    Add: CheckIdsValid method for UserServices

--- Services/Implementations/UserServices.cs
+++ Services/Implementations/UserServices.cs
@@ -1,19 +1,43 @@
 using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
+using DataBase;
+using Context;
 namespace Services;
 
 // Класс UserServices
 public class UserServices : IUserServices
 {
+    private readonly IUserRepository _UserRepository;
     private readonly IHttpContextAccessor _HttpContextAccessor;
 
-    public UserServices(IHttpContextAccessor httpContextAccessor)
+    public UserServices(IHttpContextAccessor httpContextAccessor, IUserRepository userRepository)
     {
         _HttpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        _UserRepository = userRepository;
     }
 
     public int GetMyId()
     {
         return Convert.ToInt32(_HttpContextAccessor.HttpContext.User.Claims.First(i => i.Type == ClaimTypes.NameIdentifier).Value);
+    }
+
+
+    public async Task<IBaseResponse> CheckIdsValid(List<int> ids)
+    {
+        BaseResponse response;
+
+        User tryFind;
+        for (int i = 0; i < ids.Count; i++)
+        {
+            tryFind = await _UserRepository.FirstOrDefaultAsync(x => x.Id == ids[i]);
+            if (tryFind == null)
+            {
+                response = BaseResponse.NotFound("User id not found");
+                return response;
+            }
+        }
+
+        response = BaseResponse.NoContent();
+        return response;
     }
 }
--- Services/Interfaces/IUserServices.cs
+++ Services/Interfaces/IUserServices.cs
@@ -1,7 +1,10 @@
+using DataBase;
 namespace Services;
 
 // Интерфейс IUserServices
 public interface IUserServices
 {
     int GetMyId();
+
+    Task<IBaseResponse> CheckIdsValid(List<int> ids);
 }
