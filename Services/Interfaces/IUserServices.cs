c5e9a25a5aa17bbfe3cd2ec0c054a3b77c338f6d
aragami3070 <smirnov17612@gmail.com>
2025-05-07 13:44:54 +0400

    Add: GetMyRole method in UserServices

--- Services/Interfaces/IUserServices.cs
+++ Services/Interfaces/IUserServices.cs
@@ -4,7 +4,11 @@ namespace Services;
 // Интерфейс IUserServices
 public interface IUserServices
 {
+    // Получить Id из jwt
     int GetMyId();
+
+    // Получить Role из jwt
+    string GetMyRole();
 
     Task<IBaseResponse> CheckIdsValid(List<int> ids);
 }
