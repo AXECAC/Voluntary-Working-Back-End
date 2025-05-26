73be6b95bef26228cf2c56c9aa117d2704bfd5db
Anarchy73 <kanalcraba@gmail.com>
2025-05-27 01:12:18 +0400

    Add: delete old backups

--- DataBase/Docker/scripts/cleanup_script.sh
+++ DataBase/Docker/scripts/cleanup_script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Удаляем записи старше 7 дней из таблицы posts
-PGPASSWORD=postgres psql -h db -U aragami -d voluntarydb -c "DELETE FROM posts WHERE created_at < NOW() - INTERVAL '6 months';"
+BACKUP_DIR="/backups"
+# Удаляем записи старше 6 месяцев из таблицы posts
+PGPASSWORD=password psql -h db -U aragami -d voluntarydb -c "DELETE FROM posts WHERE created_at < NOW() - INTERVAL '6 months';"
+# Удаляем бэкапы старше 2 месяцев
+find "$BACKUP_DIR" -name "*.dump" -type f -mtime +60 -delete -printf "Deleted old backup: %f\n" >> /var/log/cron.log
