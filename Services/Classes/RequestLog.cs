e29053ca1c82baa63c7dac0e2850e46bcb3aef68
aragami3070 <smirnov17612@gmail.com>
2025-05-26 15:52:57 +0400

    Add: RequestLog class

---
+++ Services/Classes/RequestLog.cs
@@ -0,0 +1,28 @@
+namespace Services;
+
+// Class RequestLog
+public class RequestLog
+{
+    public int Id { get; set; }
+    public int RequestId { get; set; }
+    public string Action { get; set; }
+    public DateTime Date { get; set; }
+
+    public RequestLog() { }
+
+    public RequestLog(int id, int requestId, string action)
+    {
+        Id = id;
+        RequestId = requestId;
+        Action = action;
+        Date = DateTime.Now;
+    }
+
+    public RequestLog(int id, int requestId, string action, DateTime date)
+    {
+        Id = id;
+        RequestId = requestId;
+        Action = action;
+        Date = date;
+    }
+}
