2f6567e8639c185ee05acc69bdf8a730276366a4
aragami3070 <smirnov17612@gmail.com>
2025-06-10 01:58:59 +0400

    Feat: update AuthServices for new jwt

--- Services/Interfaces/IAuthServices.cs
+++ Services/Interfaces/IAuthServices.cs
@@ -4,6 +4,6 @@ namespace Services;
 // Интерфейс IAuthServices
 public interface IAuthServices
 {
-    Task<IBaseResponse<string>> TryRegister(User user, string secretKey);
-    Task<IBaseResponse<string>> TryLogin(LoginUser form, string secretKey);
+    Task<IBaseResponse<Tokens>> TryRegister(User user, string secretKey);
+    Task<IBaseResponse<Tokens>> TryLogin(LoginUser form, string secretKey);
 }
