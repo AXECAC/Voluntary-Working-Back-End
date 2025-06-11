35c71d2ea6d6459570e9efa2eef85b8989d56107
aragami3070 <smirnov17612@gmail.com>
2025-06-11 12:27:47 +0400

    Feat: add usage Verify password in TryLogin in AuthServices

--- Services/Implementations/AuthServices.cs
+++ Services/Implementations/AuthServices.cs
@@ -51,18 +51,15 @@ public class AuthServices : IAuthServices
 
     public async Task<IBaseResponse<Tokens>> TryLogin(LoginUser form, string secretKey)
     {
-        // Хэширование Password
-        User user = _HashingServices.Hashing(form);
-
         BaseResponse<Tokens> baseResponse;
         // Найти user по email
-        var userDb = await _UserRepository.FirstOrDefaultAsync(x => x.Email == user.Email);
+        var userDb = await _UserRepository.FirstOrDefaultAsync(x => x.Email == form.Email);
 
         // User существует
         if (userDb != null)
         {
             // Сравнить хэш пароля
-            if (user.Password == userDb.Password)
+            if (_HashingServices.Verify(form.Password, userDb.Password))
             {
                 // Ok (200)
                 baseResponse = BaseResponse<Tokens>.Ok(data: await _TokenServices.GenerateJWTToken(userDb, secretKey));
@@ -76,6 +73,8 @@ public class AuthServices : IAuthServices
         // User не существует
         else
         {
+            // Фиктивная проверка
+            _HashingServices.Verify(form.Password);
             // Unauthorized (401)
             baseResponse = BaseResponse<Tokens>.Unauthorized("Email not found");
         }
