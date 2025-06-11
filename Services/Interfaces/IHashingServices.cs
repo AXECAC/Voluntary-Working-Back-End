7eb502a5765e3236e34c7798e26e729274636680
aragami3070 <smirnov17612@gmail.com>
2025-06-11 12:26:49 +0400

    Feat: migrate from sha512 hash algorithm to BCrypt

--- Services/Interfaces/IHashingServices.cs
+++ Services/Interfaces/IHashingServices.cs
@@ -4,6 +4,7 @@ namespace Services;
 // Интерфейс IHashingServices
 public interface IHashingServices
 {
+    bool Verify(string password, string hash);
+    bool Verify(string password);
     User Hashing(User userEntity);
-    User Hashing(LoginUser userEntity);
 }
